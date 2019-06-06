bcbfd68a04c974ba27bbd71a6f43f249b1702ea0
lichen <850279829@qq.com>
2019-06-06 22:23:26 +0800

    stat.c添加符号跟随功能

--- 2.FileProperties/stat.c
+++ 2.FileProperties/stat.c
@@ -15,6 +15,8 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
+	printf("st_mode = %d\n", s.st_mode);
+
 	//文件类型
 	char mode = 0;
 	if(S_ISLNK(s.st_mode))
@@ -43,7 +45,17 @@ int main(int argc, char* argv[])
 		else
 			perm[i] = '-';
 	}
-	printf("%c%s %ld %d %d %ld %ld %s\n", mode, perm, s.st_nlink, s.st_uid, s.st_gid, s.st_size, s.st_atime, argv[1]);
+
+	if(mode == 'l')
+	{
+		char buf[30] = {0};
+		readlink(argv[1], buf, sizeof(buf));
+		printf("%c%s %ld %d %d %ld %ld %s --> %s\n", mode, perm, s.st_nlink, s.st_uid, s.st_gid, s.st_size, s.st_atime, argv[1], buf);
+	}
+	else
+	{
+		printf("%c%s %ld %d %d %ld %ld %s\n", mode, perm, s.st_nlink, s.st_uid, s.st_gid, s.st_size, s.st_atime, argv[1]);
+	}
 
 	return 0;
 }
